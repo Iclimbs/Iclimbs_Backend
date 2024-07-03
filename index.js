e38ef46633df786f8f05b8890582c8b55b4c821a
soniaiclimbs <soniadiwedi@iclimbs.com>
2024-07-03 12:07:42 +0530

    edit in index.js

--- index.js
+++ index.js
@@ -9,15 +9,12 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use("/api/v1/", require("./routes/routes"));
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// app.use("/", (req, res) => {
-//     res.json({ Status: "Success", Message: "Welcome To Backend" })
-// })
-
-app.use("/api/v1/", require("./routes/routes"));
 
 app.listen(process.env.port, async () => {
   try {
